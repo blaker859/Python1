886a5a33edeb59638d411f849534a26a069c9ec5
blaker859 <112280753+blaker859@users.noreply.github.com>
2023-07-24 19:16:06 -0400

    day 1 finished

---
+++ Code/unpacking.py
@@ -0,0 +1,19 @@
+a = b = c = d = e = f = 10
+print(c)
+
+x, y, z = 1, 2, 76
+print(x, y, z)
+
+print("Unpacking a tuple")
+
+data = 1, 2, 76
+x, y, z = data
+print(x, y, z)
+
+print("Unpacking a list")
+
+data_list = [12,14,13]
+# data_list.append(15)
+p,q,r = data_list
+print(p,q,r)
+
