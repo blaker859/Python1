b109ffe87536e4388ed0297960b059ac5e82f85d
blaker859 <112280753+blaker859@users.noreply.github.com>
2023-07-24 18:50:28 -0400

    fixing repo

--- Code/number_list.py
+++ Code/number_list.py
@@ -1,12 +1,23 @@
+empty_list = []
 even=[2,4,6,8]
 odd=[1,3,5,7,9]
 
-print(min(even))
-print(max(even))
-print(min(odd))
-print(max(odd))
-print("\tLen Operation")
-print(len(even))
-print(len(odd))
-print()
-print("mississippi".count("s"))
+
+# print(min(even))
+# print(max(even))
+# print(min(odd))
+# print(max(odd))
+# print("\tLen Operation")
+# print(len(even))
+# print(len(odd))
+# print()
+# print("mississippi".count("s"))
+
+numbers = even + odd
+print(numbers)
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)
+print(numbers)
+
+digits = sorted("432985617")
+print(digits)
--- Code/sorting.py
+++ Code/sorting.py
@@ -1,16 +1,28 @@
-pangram = "The quick brown fox jumps over the lazy dog"
+# pangram = "The quick brown fox jumps over the lazy dog"
 
-letters= sorted(pangram)
-print(letters)
+# letters= sorted(pangram)
+# print(letters)
 
-numbers = [2.3,4.5,8.7,3.1,1.6]
-sorted_numbers= sorted(numbers)
-print(sorted_numbers)
-print(numbers)
-# another_sorted_numbers = numbers.sort()
-numbers.sort()
-print(numbers)
-# print(another_sorted_numbers)
+# numbers = [2.3,4.5,8.7,3.1,1.6]
+# sorted_numbers= sorted(numbers)
+# print(sorted_numbers)
+# print(numbers)
+# # another_sorted_numbers = numbers.sort()
+# numbers.sort()
+# print(numbers)
+# # print(another_sorted_numbers)
 
-missing_letter = sorted("The quick brown fox jumped over the lazy dog")
-print(missing_letter)
+# missing_letter = sorted("The quick brown fox jumped over the lazy dog",
+#                         key=str.casefold)
+# print(missing_letter)
+
+names = ["Graham",
+         "John",
+         "terry",
+         "erric",
+         "Terry",
+         "michael"
+         ]
+
+names.sort(key=str.casefold)
+print(names)
