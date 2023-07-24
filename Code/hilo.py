9723ea66cc7d640b32a52e7c5a700b0a64d19898
blaker859 <112280753+blaker859@users.noreply.github.com>
2023-07-24 18:36:04 -0400

    adding files

---
+++ Code/hilo.py
@@ -0,0 +1,27 @@
+low = 1
+high = 1000
+
+print("Please think of a number between {} and {}".format(low, high))
+input("Press ENTER to start")
+
+guesses = 1
+while True:
+    print("\t Guessing in the range of {} to {}".format(low, high))
+    guess = low + (high - low) // 2
+    high_low = input("My guess is {}. Should I guess High or Low? "
+                     "Enter H or L. C if correct!"
+                     .format(guess).casefold())
+    if high_low == "h":
+      # Guess higher. The low end of the range becomes 1 greater than the guess
+      low = guess + 1
+    elif high_low == "l":
+      # Guess lower. The High end of the range becomes 1 lower than the guess
+      high = guess - 1
+    elif high_low == "c":
+      print("I got it in {} guesses!".format(guesses))
+      break
+    else:
+      print("Please enter H L or C")
+    
+    # guesses = guesses +1
+    guesses += 1
---
+++ Code/lists_intro.py
@@ -0,0 +1,18 @@
+computer_parts = ["Computer",
+                  "Monitor",
+                  "keyboard",
+                  "mouse",
+                  "CPU",
+                  "RAM",
+                  "CPU Cooler",
+                  "Motherboard"
+                  ]
+
+for number, part in enumerate(computer_parts):
+    print(number +1, part)
+ 
+# print("\tSlicing a list")
+# print(computer_parts[2])
+# print(computer_parts[-1])
+# print(computer_parts[0:3])
+
